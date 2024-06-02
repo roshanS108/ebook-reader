06e3d87816a51fd904bedd867ae48ddd04faa5a0
= <roshanshoti100@gmail.com>
2024-06-02 16:05:31 -0400

    finished with all the necessary entities

--- src/main/java/com/mybook/easyread/entity/Book.java
+++ src/main/java/com/mybook/easyread/entity/Book.java
@@ -1,4 +1,27 @@
 package com.mybook.easyread.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+// Book Class
 public class Book {
+    private String title;
+    private String author;
+    private String isbn;
+    private String content;
+    private double price;
+
+    public Book(String title, String author, String isbn, String content, double price) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.content = content;
+        this.price = price;
+    }
+
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public String getISBN() { return isbn; }
+    public String getContent() { return content; }
+    public double getPrice() { return price; }
 }
--- src/main/java/com/mybook/easyread/entity/User.java
+++ src/main/java/com/mybook/easyread/entity/User.java
@@ -1,4 +1,19 @@
 package com.mybook.easyread.entity;
-
+import java.util.ArrayList;
+import java.util.List;
 public class User {
+    private String name;
+    private String email;
+    private List<Book> purchasedBooks = new ArrayList<>();
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+    public void addPurchasedBook(Book book) {
+        purchasedBooks.add(book);
+    }
+
+    public List<Book> getPurchasedBooks() {
+        return purchasedBooks;
+    }
 }
